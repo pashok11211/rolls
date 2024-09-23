01fdc90b4c4462d872716e1cabe5921b4ccb7f87
SU <gfhhjfhgfg4@mail.ru>
2024-09-23 12:14:18 +0300

    rolls

---
+++ roll.py
@@ -0,0 +1,79 @@
+class Roll:
+    def __init__(self, name: str, main_ingredient: str, rice_weight: float, seaweed_weight: float, fillings=None):
+
+        self.__name = name
+        self.__main_ingredient = main_ingredient
+        self.__rice_weight = rice_weight
+        self.__seaweed_weight = seaweed_weight
+
+        self.__fillings = fillings if fillings is not None else []
+
+    def get_name(self):
+        return self.__name
+
+    def get_main_ingredient(self):
+        return self.__main_ingredient
+
+    def get_rice_weight(self):
+        return self.__rice_weight
+
+    def get_seaweed_weight(self):
+        return self.__seaweed_weight
+
+    def get_fillings(self):
+        return self.__fillings
+
+    def set_name(self, name: str):
+        self.__name = name
+
+    def set_main_ingredient(self, main_ingredient: str):
+        self.__main_ingredient = main_ingredient
+
+    def set_rice_weight(self, rice_weight: float):
+        self.__rice_weight = rice_weight
+
+    def set_seaweed_weight(self, seaweed_weight: float):
+        self.__seaweed_weight = seaweed_weight
+
+    def add_filling(self, filling: str):
+
+        self.__fillings.append(filling)
+
+    def remove_filling(self, filling: str):
+
+        if filling in self.__fillings:
+            self.__fillings.remove(filling)
+
+    def update_recipe(self, rice_weight=None, seaweed_weight=None, fillings=None):
+
+        if rice_weight is not None:
+            self.__rice_weight = rice_weight
+        if seaweed_weight is not None:
+            self.__seaweed_weight = seaweed_weight
+        if fillings is not None:
+            self.__fillings = fillings
+
+    def create_custom_roll(self, name: str, main_ingredient: str, rice_weight: float, seaweed_weight: float, fillings):
+
+        return Roll(name, main_ingredient, rice_weight, seaweed_weight, fillings)
+
+    def __str__(self):
+        return (f"Ролл: {self.__name}, Основной ингредиент: {self.__main_ingredient}, "
+                f"Вес риса: {self.__rice_weight} г, Вес водорослей: {self.__seaweed_weight} г, "
+                f"Начинка: {', '.join(self.__fillings)}")
+
+
+if __name__ == "__main__":
+
+    my_roll = Roll(name="Калифорнийский ролл", main_ingredient="Краб", rice_weight=150, seaweed_weight=10)
+    my_roll.add_filling("Авокадо")
+    my_roll.add_filling("Огурец")
+
+    print(my_roll)
+
+
+    my_roll.update_recipe(rice_weight=160, fillings=["Авокадо", "Огурец", "Острый майонез"])
+    print(my_roll)
+
+    custom_roll = my_roll.create_custom_roll("Кастомный ролл", "Тунец", 140, 15, ["Кунжут", "Острый соус"])
+    print(custom_roll)
