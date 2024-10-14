027edfb5dd0e5eca56ed05145661916eeda9ca25
SU <gfhhjfhgfg4@mail.ru>
2024-10-14 16:46:40 +0300

    .4

--- adminview.py
+++
@@ -1,9 +0,0 @@
-class AdminView:
-    def show_admin_view(self, roll):
-        print("*** Администраторский вид ***")
-        print(f"Название: {roll.name}")
-        print(f"Основной ингредиент: {roll.main_ingredient}")
-        print(f"Вес риса: {roll.rice_weight} грамм")
-        print(f"Вес водорослей: {roll.seaweed_weight} грамм")
-        print(f"Ингредиенты: {', '.join(roll.fillings)}")
-        print(f"Изображение доступно по пути: static/images/roll.jpg")
